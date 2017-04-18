122b992684d17a3c759efad171eede6e42c58d66
Christopher N. Hesse <raymanfx@gmail.com>
2017-04-18 09:22:49 +0200

    sdm845-common: Enforce vendor tree presence
    A compiled build has no chance of booting without the prebuilt
    binaries, so fail the build if those are not present.
    
    Change-Id: Iadd43e887d426732e549969ab8deb14194ca818d

--- sdm845.mk
+++ sdm845.mk
@@ -8,9 +8,6 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Get non-open-source specific aspects
-$(call inherit-product-if-exists, vendor/xiaomi/sdm845-common/sdm845-common-vendor.mk)
-
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
@@ -546,3 +543,6 @@ PRODUCT_PACKAGES += \
 # msm_irqbalance
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/xiaomi/sdm845-common/sdm845-common-vendor.mk)
