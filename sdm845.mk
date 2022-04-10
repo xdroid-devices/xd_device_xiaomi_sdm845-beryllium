bcd95c576e786290208ac49ab262bb54048bea53
ibratabian17 <ibratabian@gmail.com>
2022-04-11 04:47:03 +0700

    sdm845-common: move IFAA & WfdCommon to extras
    platform-bootclasspath cannot depend on something from a non-root
    soong namespace.

--- sdm845.mk
+++ sdm845.mk
@@ -382,6 +382,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # WiFi Display
+-include vendor/xiaomi/sdm845-common-extras/WfdCommon.mk
+
 PRODUCT_PACKAGES += \
     libnl \
     libwfdaac_vendor
