6c7412fc34c002ce0e3da21314f7727d1552d600
ibratabian17 <ibratabian@gmail.com>
2022-04-12 09:59:56 +0700

    sdm845-common: revert some parts commit
    This reverts commit fe3eb717bf7e75102c44300192d044fbce7430be.
    This reverts commit 192d349c9a79340c66dc7189625afb434be15d77.
    This reverts commit 04206e241705bc2aa6eb8c8b2f91727cb9dcfb37.

--- parts/src/org/lineageos/settings/thermal/ThermalService.java
+++ parts/src/org/lineageos/settings/thermal/ThermalService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020-2021 The LineageOS Project
+ * Copyright (C) 2020 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,11 +17,12 @@
 package org.lineageos.settings.thermal;
 
 import android.app.ActivityManager;
-import android.app.ActivityTaskManager;
-import android.app.ActivityTaskManager.RootTaskInfo;
-import android.app.IActivityTaskManager;
+import android.app.ActivityManager.StackInfo;
+import android.app.IActivityManager;
 import android.app.TaskStackListener;
+import android.app.ActivityTaskManager;
 import android.app.Service;
+import android.content.ComponentName;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -37,8 +38,7 @@ public class ThermalService extends Service {
 
     private String mPreviousApp;
     private ThermalUtils mThermalUtils;
-
-    private IActivityTaskManager mActivityTaskManager;
+    private IActivityManager mIActivityManager;
 
     private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
         @Override
@@ -52,12 +52,12 @@ public class ThermalService extends Service {
     public void onCreate() {
         if (DEBUG) Log.d(TAG, "Creating service");
         try {
-            mActivityTaskManager = ActivityTaskManager.getService();
-            mActivityTaskManager.registerTaskStackListener(mTaskListener);
+            ActivityTaskManager.getService().registerTaskStackListener(mTaskListener);
         } catch (RemoteException e) {
             // Do nothing
         }
         mThermalUtils = new ThermalUtils(this);
+        mIActivityManager = ActivityManager.getService();
         registerReceiver();
         super.onCreate();
     }
@@ -83,15 +83,14 @@ public class ThermalService extends Service {
         @Override
         public void onTaskStackChanged() {
             try {
-                final RootTaskInfo info = mActivityTaskManager.getFocusedRootTaskInfo();
-                if (info == null || info.topActivity == null) {
-                    return;
-                }
-
-                String foregroundApp = info.topActivity.getPackageName();
-                if (!foregroundApp.equals(mPreviousApp)) {
-                    mThermalUtils.setThermalProfile(foregroundApp);
-                    mPreviousApp = foregroundApp;
+                final StackInfo focusedStack = mIActivityManager.getFocusedStackInfo();
+                if (focusedStack != null && focusedStack.topActivity != null) {
+                    ComponentName taskComponentName = focusedStack.topActivity;
+                    String foregroundApp = taskComponentName.getPackageName();
+                    if (!foregroundApp.equals(mPreviousApp)) {
+                        mThermalUtils.setThermalProfile(foregroundApp);
+                        mPreviousApp = foregroundApp;
+                    }
                 }
             } catch (RemoteException ignored) {
             }
