eaae0bea577b336e7997269a9c6a1db391348c92
ibratabian17 <71920907+ibratabian17@users.noreply.github.com>
2022-04-12 09:53:31 +0700

    sdm845-common: comment the WfdCommon

--- sdm845.mk
+++ sdm845.mk
@@ -382,11 +382,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # WiFi Display
--include vendor/xiaomi/sdm845-common-extras/WfdCommon.mk
-
 PRODUCT_PACKAGES += \
     libnl \
     libwfdaac_vendor
 
-PRODUCT_BOOT_JARS += \
-    WfdCommon
