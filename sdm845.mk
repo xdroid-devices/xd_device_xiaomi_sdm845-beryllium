be923a4c8348693bd8ae4be1189ce1782d58aac1
ibratabian17 <ibratabian@gmail.com>
2022-06-02 13:14:10 +0000

    sdm845-common: build vendor.qti.hardware.btconfigstore
     linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti": library "vendor.qti.hardware.btconfigstore@1.0.so" not found: needed by main executable

--- sdm845.mk
+++ sdm845.mk
@@ -140,6 +140,9 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0.vendor \
     audio.bluetooth.default \
     libbthost_if \
+    \
+    vendor.qti.hardware.btconfigstore@1.0.vendor \
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor
 
 # Camera
