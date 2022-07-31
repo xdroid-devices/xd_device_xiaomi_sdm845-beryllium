f1a4c23b08cea3a128b68294477de780af9a77cc
UtsavBalar1231 <utsavbalar1231@gmail.com>
2022-07-31 16:33:16 +0600

    sdm845-common: Build some f2fs tools

--- sdm845.mk
+++ sdm845.mk
@@ -238,6 +238,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.0.vendor
     
+# F2FS
+PRODUCT_PACKAGES += \
+    sg_write_buffer \
+    f2fs_io \
+    check_f2fs
+
 # Google Photos
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/pixel/pixel_2016_exclusive.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/pixel_2016_exclusive.xml
