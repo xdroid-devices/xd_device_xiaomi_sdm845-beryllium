4de7b986fdf9994b5ff3a8ccf2f4f1a944a27953
ibratabian17 <ibratabian@gmail.com>
2022-10-02 12:49:18 +0000

    sdm845-common: Compile libldacBT_bco.so * E a2dp_vendor_ldac_decoder: A2DP_VendorLoadDecoderLdac: cannot open LDAC decoder library libldacBT_bco.so: dlopen failed: library libldacBT_bco.so not found

--- sdm845.mk
+++ sdm845.mk
@@ -138,7 +138,7 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0.vendor \
     audio.bluetooth.default \
     libbthost_if \
-    \
+    libldacBT_bco \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor
